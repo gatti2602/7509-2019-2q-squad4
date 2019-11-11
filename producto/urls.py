82b5e9ec7c4f493c765d9998a8a09c7d8c12892f
Nicolas Gatti <gatti2602@gmail.com>
2019-11-10 21:16:47 -0300

    Create de Features

--- producto/urls.py
+++ producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'producto'
 urlpatterns = [
-    path('feature', views.feature_list, name='feature_list')
+    path('feature/', views.index_feature, name='index_feature'),
+    path('feature/new/', views.post_feature, name='post_feature'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,8 +1,37 @@
-from django.http import HttpResponse
+from datetime import datetime
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
 from .models import Feature
 
 
-def feature_list(request):
+def index_feature(request):
     feature_list = Feature.objects.all()
-    output = ', '.join([f.description for f in feature_list])
-    return HttpResponse(output)
+    context = {
+        'features_list': feature_list,
+    }
+    return render(request, 'producto/index_feature.html', context)
+
+
+def post_feature(request):
+    if len(request.POST) == 0:
+        return render(request, 'producto/post_feature.html', {})
+
+    try:
+        new_feature = Feature()
+        new_feature.description = request.POST['description']
+        new_feature.status = request.POST['status']
+        new_feature.expected_delivery = datetime.strptime(request.POST['expected_date'], '%Y-%m-%d')
+    except Exception as e:
+        # Redisplay the question voting form.
+        return render(request, 'producto/post_feature.html', {
+            'error_message': e,
+        })
+    else:
+        new_feature.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('producto:index_feature', args=()))
