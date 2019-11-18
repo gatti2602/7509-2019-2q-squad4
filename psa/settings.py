fc75bf68071d87b77740b74af797bed44a97dde8
Nicolas Gatti <gatti2602@gmail.com>
2019-11-18 12:15:44 -0300

    Integracion con postgres par aunificar una sola BD

--- psa/settings.py
+++ psa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,10 +77,24 @@ WSGI_APPLICATION = 'psa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE" : "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         "TEST": {
+#             "NAME":'test_db'
+#         },
+#     },
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "ENGINE" : "django.db.backends.postgresql",
+        "NAME": 'psa',
+        "USER": 'gatti2602',
+        'PASSWORD': 'paralelepipedo',
+        'HOST': 'localhost',
+        'PORT': '',
     },
 }
 
@@ -121,5 +136,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
