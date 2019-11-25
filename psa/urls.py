efa41609689cd4fa4ed3a7d83043c54b1e472e4d
Nicolas Gatti <gatti2602@gmail.com>
2019-11-24 22:15:24 -0300

    url base apunta a proyectos

--- psa/urls.py
+++ psa/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('helloworld/', include('helloworld.urls')),
     path('producto/', include('producto.urls')),
-
+    path('', RedirectView.as_view(url=reverse_lazy('proyecto:index'), permanent=False)),
     path('proyecto/', include('proyecto.urls')),
     path('admin/', admin.site.urls),
 ]
